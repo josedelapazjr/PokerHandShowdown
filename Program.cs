ed924e34f9bbbed3ddc7c0af4f1275565938323a
Jose de la Paz Jr <jwataru@gmail.com>
2019-04-01 22:04:09 -0500

    Add simple validation.

--- Program.cs
+++ Program.cs
@@ -174,21 +174,27 @@ namespace PokerHandShowdown
     public class HandEvaluate {
         private const int NUMBER_OF_CARDS =  5;
         public static List<CardGroup> generateGrouping(List<Card> cardList) {
-            cardList.Sort();
-            var cardGroupList = isFlush(cardList) 
-                ? new List<CardGroup> { new CardGroup(PokerHand.Flush, cardList )}
-                : cardList.GroupBy(card => card.Rank).Select( group => {
-                    List<Card> cardListPerGroup = group.ToList();
-                    if(isThreeOfAKind(cardListPerGroup)) return new CardGroup(PokerHand.ThreeOfAKind, cardListPerGroup);
-                    if(isPair(cardListPerGroup)) return new CardGroup(PokerHand.OnePair, cardListPerGroup);
-                    return new CardGroup(PokerHand.HighCard, cardListPerGroup);    
-            }).ToList(); 
-            cardGroupList.Sort();
-            return cardGroupList;
+            if(isValidCardList(cardList)) {
+                cardList.Sort();
+                var cardGroupList = isFlush(cardList) 
+                    ? new List<CardGroup> { new CardGroup(PokerHand.Flush, cardList )}
+                    : cardList.GroupBy(card => card.Rank).Select( group => {
+                        List<Card> cardListPerGroup = group.ToList();
+                        if(isThreeOfAKind(cardListPerGroup)) return new CardGroup(PokerHand.ThreeOfAKind, cardListPerGroup);
+                        if(isPair(cardListPerGroup)) return new CardGroup(PokerHand.OnePair, cardListPerGroup);
+                        return new CardGroup(PokerHand.HighCard, cardListPerGroup);    
+                }).ToList(); 
+                cardGroupList.Sort();
+                return cardGroupList;   
+            }
+            return new List<CardGroup>();
         } 
 
         public static bool isFlush(List<Card> cardList) {
             return cardList.GroupBy(card => card.Rank).Count() == NUMBER_OF_CARDS && cardList.GroupBy(card => card.Suit).Count() == 1;
+        }
+        public static bool isValidCardList(List<Card> cardList) {
+            return cardList.GroupBy(card => new { card.Rank, card.Suit}).Where(group => group.Count() > 1).Count() == 0;
         }
 
         private static bool isThreeOfAKind(List<Card> cardList) {
@@ -227,7 +233,7 @@ namespace PokerHandShowdown
 // CardsOnHand ( Array of Cards, isFlush(), isThreeOfAKind, isHighCard) => OK
 // CardsOnHandEvaluator => ? (Tools/Utility class)
 // Test => ?
-// Validator => ?
+// Validator => Just check duplicates
 // Tie => ?
 // HighCard => OK
 // Same Cards => OK
